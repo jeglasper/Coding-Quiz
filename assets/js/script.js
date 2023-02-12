9465d0e119c66b3504f43a01e3393a4318e3553a
jeglasper <109081362+jeglasper@users.noreply.github.com>
2023-02-12 08:57:52 -0600

    add files, initial css styling & html structure

---
+++ assets/js/script.js
@@ -0,0 +1,50 @@
+var Question1 = {
+    Question: 'What tag is used to define the metadata about an HTML document, and must always be included inside the element?',
+    MultipleChoice: ['1. <meta>', '2. <div>', '3. <table>', '4. <img>']
+}
+
+var Question2 = {
+    Question: 'Which method can be used to return a string in upper case letters?',
+    MultipleChoice: ['1. tuc()', '2. toUpperCase()', '3. touppercase()', '4. upperCase()'],
+}   
+
+var Question3 = {
+    Question: 'What declaration MUST be included as the first item in an HTML document before the tag and is used to provide instructions to the web browser?',
+    MultipleChoice: ['1. <caption>', '2. <embed>', '3. <code>', '4. <!DOCTYPE>'],
+}
+
+var Question4 = {
+    Question: 'Every HTML page must include a reference to the external file sheet file inside the ____ element.',
+    MultipleChoice: ['1. <div>', '2. Link', '3. Body', '4. Footer'],
+}
+
+var Question5 = {
+    Question: 'Where is the JavaScript placed inside an HTML document or page?',
+    MultipleChoice: ['1. In the <body> and <head> sections.', '2. In the <meta> section.', '3.  In the <title> section.', '4. In the <footer> section.'],
+}
+
+var Question6 = {
+    Question: 'What is the name of CSS design that calls for fluid and adaptable elements based on the device resolution or size?',
+    MultipleChoice: ['1. Evolution', '2. Shifting', '3. Responsive', '4. Cascading'],
+}
+
+var Question7 = {
+    Question: 'What is the type of loop that continues through a block of code as long as the specified condition remains TRUE?',
+    MultipleChoice: ['1. While Loop', '2. For Loop', '3. Else Loop', '4. Conditional Loop'],
+}
+
+var Question8 = {
+    Question: 'What is the type of loop that continues through a block of code as long as the specified condition remains TRUE?',
+    MultipleChoice: ['1. ', '2. ', '3. Conditional Loop', '4. '],
+}
+
+var Question9 = {
+    Question: 'What is the CSS property that sets the size of the whitespace outside the borders of the content?',
+    MultipleChoice: ['1. Spacer', '2. Line', '3. Block-level', '4. Margin'],
+}
+
+var Question10 = {
+    Question: 'What tag defines the body of the HTML document, and usually includes all the contents such as the text, hyperlinks, images, tables, lists, and more?',
+    MultipleChoice: ['1. <tile></title>', '2. <body></body>', '3. <br></br>', '4. <head></head>'],
+}
+
